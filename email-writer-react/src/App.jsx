662216ed5a6eaaf6e87587cc4f4385fbf7244d8f
Vattam Sneha Latha <vattamsneha2004@gmail.com>
2025-07-13 00:07:30 +0530

    added front end

--- email-writer-react/src/App.jsx
+++ email-writer-react/src/App.jsx
@@ -2,33 +2,110 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import TextField from '@mui/material/TextField';
+import Box from '@mui/material/Box';
+import MenuItem from '@mui/material/MenuItem';
+import InputLabel from '@mui/material/InputLabel';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import Button from '@mui/material/Button';
+import CircularProgress from '@mui/material/CircularProgress';
+import axios from 'axios';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [emailContent, setEmailContent]=useState('');
+  const [tone, setTone]=useState('');
+  const [generatedReply, setGeneratedReply]=useState('');
+  const [loading, setLoading]=useState(false);
+  const [error,setError]=useState('');
+  const handleSubmit = async()=>{
+    setLoading(true);
+    setError('');
+    try{
+      const response=await axios.post("http://localhost:8080/api/email/generate",{
+        emailContent,
+        tone
+      });
+      setGeneratedReply(typeof response.data==='string'?response.data:JSON.stringify(response.data));
+    }catch(error){
+      setError('Failed to generate email reply Please try again');
+      console.error(error);
+    }finally{
+      setLoading(false);
+    }
+  };
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+      <Container maxWidth="md" sx={{ py: 8}}>
+        <Typography variant='h3' component='h1' gutterBottom>
+          Email reply generator
+        </Typography>
+        <Box sx={{mx:3}}>
+          <TextField 
+              fullWidth
+              multiline
+              rows={7}
+              variant='outlined'
+              label='Original Email Content'
+              value={emailContent || ''}
+              onChange={(e)=> setEmailContent(e.target.value)}
+              sx={{mb:4}}
+              />
+              <FormControl fullWidth sx={{mb:2}}>
+                <InputLabel>Tone (Optional)</InputLabel>
+                <Select
+                value={tone||''}
+                label={"Tone (Optional"}
+                onChange={(e)=>setTone(e.target.value)}
+                >
+                  <MenuItem value="">None</MenuItem>
+                  <MenuItem value="professional">Professional</MenuItem>
+                  <MenuItem value="casual">Casual</MenuItem>
+                  <MenuItem value="friendly">Friendly</MenuItem>
+                </Select>
+              </FormControl>
+              <Button
+              variant='contained'
+              onClick={handleSubmit}
+              disabled={!emailContent|| loading} 
+              fullWidth
+              >
+                {loading?<CircularProgress size={24}/> : "Generate Reply"}
+              </Button>
+        </Box>
+        {error &&(
+          <Typography color='error' sx={{mb:2}}>
+            {error}
+          </Typography>
+        )
+        }
+
+        {generatedReply &&(
+          <Box sx={{mt:3}}>
+            <Typography variant='h6' gutterBottom>
+              Generated Reply:
+            </Typography>
+            <TextField
+            fullWidth
+            multiline
+            rows={6}
+            variant='outlined'
+            value={generatedReply||''}
+            inputProps={{readOnly: true}}
+            ></TextField>
+            <Button
+            variant="outlined"
+            sx={{mt:2}}
+            onClick={()=>navigator.clipboard.writeText(generatedReply)}
+            >
+              Copt to Clipboard
+            </Button>
+          </Box>
+        )}
+      </Container>
   )
 }
 
--- src/main/java/com/example/email_writer/controller/EmailGeneratorController.java
+++ src/main/java/com/example/email_writer/controller/EmailGeneratorController.java
@@ -1,6 +1,7 @@
 package com.example.email_writer.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +15,7 @@ import lombok.AllArgsConstructor;
 @RestController
 @RequestMapping("/api/email")
 @AllArgsConstructor
+@CrossOrigin(origins="*") // because frontend is from http://localhost:5173/ and back is 8080 port to in order to allow communicate between two different origin we use cross origin and * indicates allow communication for all types of ports and origins 
 public class EmailGeneratorController {
     private final EmailGeneratorService emailGeneratorService;
 
